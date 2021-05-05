578a6ac8471dcc576efda50695e46ecf8c460e49
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-05-05 11:19:06 +0200

    Removed Several warnings in QLever and Stxxl.

--- foxxll/io/request_interface.hpp
+++ foxxll/io/request_interface.hpp
@@ -48,22 +48,22 @@ public:
 protected:
     virtual void notify_waiters() = 0;
 
-protected:
+   protected:
     virtual void completed(bool canceled) = 0;
 
-public:
+   public:
     request_interface() = default;
 
     //! non-copyable: delete copy-constructor
     request_interface(const request_interface&) = delete;
     //! non-copyable: delete assignment operator
-    request_interface& operator = (const request_interface&) = delete;
+    request_interface& operator=(const request_interface&) = delete;
     //! moveable: default move-constructor
     request_interface(request_interface&&) = default;
     //! moveable: default move-assignment
-    request_interface& operator = (request_interface&&) = default;
+    request_interface& operator=(request_interface&&) = default;
 
-  virtual ~request_interface() { }
+    virtual ~request_interface() {}
 
     //! Suspends calling thread until completion of the request.
     virtual void wait(bool measure_time = true) = 0;
